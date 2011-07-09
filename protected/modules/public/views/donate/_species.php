ec3711b14fdd1589639aa7d4eb28ccd0bdb2a2bd
liuzhen <zh.leou@gmail.com>
2011-07-09 13:07:35 +0800

    deleted some files

--- protected/modules/public/views/donate/_species.php
+++
@@ -1,6 +0,0 @@
-<?php foreach($species as $aSpecies): ?>
-<div class="species">
-	<?php echo $aSpecies->name ?>
-	<?php echo $aSpecies->author ?>
-</div>
-<?php endforeach; ?>
--- protected/modules/public/views/donate/showSpecies.php
+++
@@ -1,2 +0,0 @@
-<h1>Show Species</h1>
-<?php $this->renderPartial('_species', array('species'=>$species)); ?>
